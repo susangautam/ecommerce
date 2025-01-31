8db25d277ac7ba6107d9baadfdba95b741099d0e
susangautam <off.susangautam@gmail.com>
2025-01-31 11:26:44 +0545

    added products and categories

--- src/data/categories.js
+++ src/data/categories.js
@@ -1,8 +1,17 @@
 const categories = [
-    { id: 1, name: "Electronics" },
-    { id: 2, name: "Fashion" },
-    { id: 3, name: "Home & Kitchen" }
-  ];
-  
-  export default categories;
-  
+  { id: 1, name: "Electronics" },
+  { id: 2, name: "Fashion" },
+  { id: 3, name: "Home & Kitchen" },
+  { id: 4, name: "Sports & Outdoors" },
+  { id: 5, name: "Beauty & Personal Care" },
+  { id: 6, name: "Books" },
+  { id: 7, name: "Toys & Games" },
+  { id: 8, name: "Automotive" },
+  { id: 9, name: "Health & Wellness" },
+  { id: 10, name: "Grocery" },
+  { id: 11, name: "Furniture" },
+  { id: 12, name: "Pet Supplies" },
+  { id: 13, name: "Music & Instruments" }
+];
+
+export default categories;
--- src/data/products.js
+++ src/data/products.js
@@ -1,7 +1,16 @@
 const products = [
-    { id: 1, name: "Laptop", price: 33000, category: "Electronics", image: "/public/laptop1.jpg" },
-    { id: 2, name: "Headphones", price: 7999, category: "Electronics", image: "headphones1.jpg" },
-    { id: 3, name: "Shoes", price: 79, category: "Fashion", image: "https://via.placeholder.com/150" }
+    { id: 1, name: " Dell Laptop", price: 33000, category: "Electronics", image: "/public/laptop1.jpg" },
+    { id: 2, name: "Sonos Ace Headphones", price: 7999, category: "Electronics", image: "headphones1.jpg" },
+    { id: 3, name: "Adidas Shoes", price: 4900  , category: "Fashion", image: "shoe1.jpg" },
+    { id: 4, name: "Dirt Bike Helmet", price: 2100  , category: "Automotive", image: "automotive1.jpg" },
+    { id: 5, name: "Ordinary Makeup Kit", price: 6500  , category: "Beauty & Personal Care", image: "beauty1.jpg" },
+    { id: 6, name: "Football", price: 900  , category: "Sports & Outdoors", image: "football1.jpg" },
+    { id: 7, name: "Wooden Chair", price: 3100  , category: "Furniture", image: "furniture1.jpg" },
+    { id: 8, name: "Dawn Cleaner", price: 199  , category: "Grocery", image: "grocery1.jpg" },
+    { id: 9, name: "Yamaha Guitar", price: 21900  , category: "Music & Instruments", image: "music1.jpg" },
+    { id: 10,name: "Non Sticky Pan", price: 1300  , category: "Home & Kitchen", image: "pan1.jpg" },
+    { id: 11,name: "Pedigree Pet Food", price: 700  , category: "Pet Supplies", image: "pets1.jpg" },
+    { id: 12,name: "Tweety Doll for Children", price: 4900  , category: "Toys & Games", image: "toy1.jpg" },
   ];
   
   export default products;
